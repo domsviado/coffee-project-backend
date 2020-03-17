5dfc3f591665b456f007af97f324952b492e81b7
Dominic <viado.dominic.n@gmail.com>
2020-03-18 02:14:02 +0800

    add products module

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const { admin, auth } = require('../../middleware/auth');
 const CustomerController = require('../../controllers/CustomerController');
+const ProductController = require('../../controllers/ProductController');
 
 const router = express.Router();
 
 router.get('/customers/detail', auth, CustomerController.getCustomer);
 router.post('/customers/create', CustomerController.createCustomer);
 router.post('/customers/update', auth, CustomerController.updateCustomer);
+
+router.get('/products', auth, ProductController.getAllProducts);
+router.post('/products/create', admin, ProductController.createProduct);
 
 module.exports = router;
