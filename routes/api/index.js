b4d05d5fbe4b1acd053630c72f96354ce1e55a43
Dominic <viado.dominic.n@gmail.com>
2020-03-18 02:24:47 +0800

    add admin module

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,7 @@ const { admin, auth } = require('../../middleware/auth');
 const CustomerController = require('../../controllers/CustomerController');
 const ProductController = require('../../controllers/ProductController');
 const OrderController = require('../../controllers/OrderController');
+const AdminController = require('../../controllers/AdminController');
 
 const router = express.Router();
 
@@ -15,5 +16,7 @@ router.post('/products/create', admin, ProductController.createProduct);
 
 router.post('/orders/create', auth, OrderController.createOrder);
 router.post('/orders/update', auth, OrderController.updateOrder);
+
+router.post('/admin/create', AdminController.createAdmin);
 
 module.exports = router;
